22c164ef0b3b706aed539145676794a3d6f09f60
suraj singh <surajthakurrs45@gmail.com>
2023-04-21 10:13:36 +0530

    1.0.0

---
+++ js/SearchBar.js
@@ -0,0 +1,306 @@
+let searchData=[
+{
+ "suggestion":"Introduction to DBMS",
+ "description":"This is intro part of DBMS",
+ "path":"../pages/IntroductionToDBMS.html"
+},
+{
+ "suggestion":"Installation of DBMS",
+ "description":"This is we get to know about installation of dbms",
+ "path":"../pages/Installation.html"
+},
+{
+ "suggestion":"DDL command",
+ "description":"DDL commands in DBMS",
+ "path":"../pages/DDL.html"
+},
+{
+ "suggestion":"DML command",
+ "description":"DML commands in DBMS",
+ "path":"../pages/DML.html"
+},
+{
+ "suggestion":"DCL command",
+ "description":"DCL commands in DBMS",
+ "path":"../pages/DCL.html"
+},
+{
+ "suggestion":"DQL command",
+ "description":"DQL commands in DBMS",
+ "path":"../pages/DQL.html"
+},
+{
+ "suggestion":"TCL command",
+ "description":"TCL commands in DBMS",
+ "path":"../pages/TCL.html"
+},
+{
+ "suggestion":"NOT NULL command",
+ "description":"NOT NULL commands in DBMS",
+ "path":"../pages/NOTNULL.html"
+},
+{
+ "suggestion":"Primary Key command",
+ "description":"Primary Key commands in DBMS",
+ "path":"../pages/PrimaryKey.html"
+},
+{
+ "suggestion":"Foreign Key command",
+ "description":"Foreign Key commands in DBMS",
+ "path":"../pages/ForeignKey.html"
+},
+{
+ "suggestion":"Check Constraint",
+ "description":"Check Constraint in DBMS",
+ "path":"../pages/CHECK.html"
+},
+{
+ "suggestion":"DEFAULT Constraint",
+ "description":"DEFAULT Constraint in DBMS",
+ "path":"../pages/Default.html"
+},
+
+{
+ "suggestion":"DATE Function",
+ "description":"DATE Function in DBMS",
+ "path":"../pages/DateFunction.html"
+},
+
+{
+ "suggestion":"String Function",
+ "description":"String Function in DBMS",
+ "path":"../pages/StringFunction.html"
+},
+
+{
+ "suggestion":"NUMERIC Function",
+ "description":"NUMERIC Function in DBMS",
+ "path":"../pages/NumericFunction.html"
+},
+
+
+{
+  "suggestion":"Introduction to PL SQL",
+ "description":"Start learning PL SQL ",
+ "path":"../pages/PLIntro.html"
+},
+
+{
+  "suggestion":"ADVANTAGES to PL SQL",
+ "description":"Start learning ADVANTAGES of PL SQL ",
+ "path":"../pages/AdvanatgeOFPL.html"
+},
+{
+  "suggestion":"VARIABLES AND CONSTANTS",
+ "description":"Start learning VARIABLES AND CONSTANTS of PL SQL ",
+ "path":"../pages/VariableAndConstraint.html"
+},
+{
+  "suggestion":"CONTROL STATEMENTS",
+ "description":"Start learning CONTROL STATEMENTS of PL SQL ",
+ "path":"../pages/ControlStatement.html"
+},
+{
+  "suggestion":"ITERAIVE CONTROL",
+ "description":"Start learning ITERAIVE CONTROL of PL SQL ",
+ "path":"../pages/IterativeControl.html"
+},
+{
+  "suggestion":"CONDITIONAL CONTROL",
+ "description":"Start learning CONDITIONAL CONTROL of PL SQL ",
+ "path":"../pages/ConditionControl.html"
+},
+{
+  "suggestion":"OVERVIEW OF ERROR HANDLING",
+ "description":"Start learning OVERVIEW OF ERROR HANDLING IN PL SQL ",
+ "path":"../pages/OverviewErrorHandling.html"
+},
+{
+  "suggestion":"EXCEPTION TYPES",
+ "description":"Start learning EXCEPTION TYPES IN PL SQL ",
+ "path":"../pages/ExceptionTypes.html"
+},
+{
+  "suggestion":"INTRODUCTION To Subprogram",
+ "description":"Start learning INTRODUCTION To Subprogram IN PL SQL ",
+ "path":"../pages/SubProgramIntro.html"
+},
+{
+  "suggestion":"Types of Subprogram",
+ "description":"Start learning Types of Subprogram IN PL SQL ",
+ "path":"../pages/SubprogramTYPES.html"
+},
+{
+  "suggestion":"Advantage of Subprogram",
+ "description":"Start learning Advantage of Subprogram IN PL SQL ",
+ "path":"../pages/SubprogramAdvanatge.html"
+},
+{
+  "suggestion":"Procedure of Subprogram",
+ "description":"Start learning Procedure of Subprogram IN PL SQL ",
+ "path":"../pages/SubprogramProcedure.html"
+},
+{
+  "suggestion":"Functions of Subprogram",
+ "description":"Start learning Functions of Subprogram IN PL SQL ",
+ "path":"../pages/SubprogramFunction.html"
+},
+{
+  "suggestion":"Package of Subprogram",
+ "description":"Start learning Package of Subprogram IN PL SQL ",
+ "path":"../pages/SubprogramPackage.html"
+},
+{
+  "suggestion":"Introduction to Triggers",
+ "description":"Start learning Introduction to Triggers IN PL SQL ",
+ "path":"../pages/TriggerIntroduction.html"
+},
+{
+  "suggestion":"Uses of Triggers",
+ "description":"Start learning Uses of Triggers IN PL SQL ",
+ "path":"../pages/TriggarUse.html"
+},
+{
+  "suggestion":"Parts of Triggers",
+ "description":"Start learning Parts of Triggers IN PL SQL ",
+ "path":"../pages/TriggarPart.html"
+},
+{
+  "suggestion":"Types of Triggers",
+ "description":"Start learning Types of Triggers IN PL SQL ",
+ "path":"../pages/TriggerTypes.html"
+},
+{
+  "suggestion":"SYNTAX of Triggers",
+ "description":"Start learning SYNTAX of Triggers IN PL SQL ",
+ "path":"../pages/TriggerSyntax.html"
+},
+{
+  "suggestion":"ERRORS of Triggers",
+ "description":"Start learning ERRORS of Triggers IN PL SQL ",
+ "path":"../pages/ErrorWithTrigger.html"
+},
+{
+  "suggestion":"Introduction To DBMS",
+ "description":"Introduction to dbms gives basic knowledge of dbms ",
+ "path":"../pages/IntroductionToDBMS.html"
+},
+{
+  "suggestion":"Truncate Command",
+ "description":"Truncate command in SQL",
+ "path":"../pages/TruncateDDL.html"
+},
+{
+  "suggestion":"Create Command",
+ "description":"Create command in SQL",
+ "path":"../pages/CreateDDL.html"
+},
+{
+  "suggestion":"DROP Command",
+ "description":"DROP command in SQL",
+ "path":"../pages/DropDDL.html"
+},
+{
+  "suggestion":"ALter Command",
+ "description":"Alter command in SQL",
+ "path":"../pages/ALterDDL.html"
+},
+{
+  "suggestion":"Rename Command",
+ "description":"Rename command in SQL",
+ "path":"../pages/RenameDDL.html"
+},
+
+
+{
+  "suggestion":"Update Command",
+ "description":"Update command in SQL",
+ "path":"../pages/UpdateDML.html"
+},
+{
+  "suggestion":"Insert Command",
+ "description":"Insert command in SQL",
+ "path":"../pages/InsertDML.html"
+},
+{
+  "suggestion":"Delete Command",
+ "description":"Delete command in SQL",
+ "path":"../pages/DeleteDML.html"
+},
+
+
+{
+  "suggestion":"Grant Command",
+ "description":"Grant command in SQL",
+ "path":"../pages/GrantDcl.html"
+},
+{
+  "suggestion":"Revoke Command",
+ "description":"Revoke command in SQL",
+ "path":"../pages/RevokeDcl.html"
+},
+{
+  "suggestion":"Select Command",
+ "description":"Select command in SQL",
+ "path":"../pages/SelectDQL.html"
+},
+
+
+{
+  "suggestion":"Commit Command",
+ "description":"Commit command in SQL",
+ "path":"../pages/CommitTcl.html"
+},
+{
+  "suggestion":"Rollback Command",
+ "description":"Rollback command in SQL",
+ "path":"../pages/RollbackTcl.html"
+},
+{
+  "suggestion":"Savepoint Command",
+ "description":"Savepoint command in SQL",
+ "path":"../pages/SavepointTcl.html"
+},
+
+{
+  "suggestion":"Quick Start",
+ "description":"Start learning DBMS ",
+ "path":"../pages/QuickStart.html"
+}
+]
+
+const searchInput=()=>{
+const search=document.getElementById('search').value;
+const learn=document.getElementById('learn');
+
+
+let result=searchData.filter((item)=>{
+return item.suggestion.toLowerCase().includes(search.toLowerCase())
+})
+
+if(result.length==0){
+learn.style.display="none";
+
+document.getElementById('sugestion').innerHTML=`<a><div class="link">
+<span>#</span>
+<h4>NO RESULT FOUND</h4>
+<p>-------</p>
+</div></a>`
+
+}else{
+learn.style.display="block";
+document.getElementById('sugestion').innerHTML=""
+
+for(let i=0;i<result.length;i++){
+document.getElementById('sugestion').innerHTML+=`<a href=${result[i].path}><div class="link">
+<span>#</span>
+<h4>${result[i].suggestion}</h4>
+<p>${result[i].description}</p>
+<img src="../image/uturn.png" alt="u turn icon" id="uturn">
+</div></a>`
+}
+}
+}
+
+
+
